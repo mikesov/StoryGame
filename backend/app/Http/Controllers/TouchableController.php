aa250e0cf3b37164c499f2d12b09b2ee05b459e4
mikesov <minh.bui@monkey.edu.vn>
2023-08-10 09:14:20 +0700

    Rename some Controllers

--- backend/app/Http/Controllers/TouchableController.php
+++ backend/app/Http/Controllers/TouchableController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class BlinkWordController extends Controller
+class TouchableController extends Controller
 {
     //
 }
