d0dd622c2339c2ab2d860a13cd36c8b340a588b2
mikesov <minh.bui@monkey.edu.vn>
2023-08-11 17:28:19 +0700

    Add StoryRepositoryInterface, add method find

--- backend/app/Contracts/RepositoryInterfaces/StoryRepositoryInterface.php
+++ backend/app/Contracts/RepositoryInterfaces/StoryRepositoryInterface.php
@@ -5,4 +5,5 @@ namespace App\Contracts\RepositoryInterfaces;
 interface StoryRepositoryInterface extends AbstractRepositoryInterface
 {
     public function getAll();
+    public function find($id);
 }
--- backend/app/Contracts/RepositoryInterfaces/UserRepositoryInterface.php
+++ backend/app/Contracts/RepositoryInterfaces/UserRepositoryInterface.php
@@ -5,4 +5,5 @@ namespace App\Contracts\RepositoryInterfaces;
 interface UserRepositoryInterface extends AbstractRepositoryInterface
 {
     public function getAll();
+    public function find($id);
 }
