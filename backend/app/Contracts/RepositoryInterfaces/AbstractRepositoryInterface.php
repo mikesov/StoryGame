15197a4ac02eba9f363366cb9cf3403d79dba150
mikesov <minh.bui@monkey.edu.vn>
2023-08-15 17:09:46 +0700

    Change argument of update method

--- backend/app/Contracts/RepositoryInterfaces/AbstractRepositoryInterface.php
+++ backend/app/Contracts/RepositoryInterfaces/AbstractRepositoryInterface.php
@@ -7,6 +7,6 @@ interface AbstractRepositoryInterface
     public function getAll();
     public function find($id);
     public function store($attributes = []);
-    public function update($id, $attributes = []);
+    public function update(int $id, array $attributes = []);
     public function delete($id);
 }
