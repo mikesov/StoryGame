d3d3135186658928ee44e6c5dd7e363e7bfd373e
mikesov <minh.bui@monkey.edu.vn>
2023-08-21 10:19:16 +0700

    Reformat

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -25,7 +25,7 @@ Route::middleware([
     })->name('dashboard');
 });
 
-    Auth::routes();
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
