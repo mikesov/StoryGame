fc10a727cd9b17e2e5fab3e62e2c876508ce21bd
mikesov <minh.bui@monkey.edu.vn>
2023-08-23 11:59:56 +0700

    Load fonts

--- frontend/app/_layout.js
+++ frontend/app/_layout.js
@@ -1,27 +1,31 @@
 import { Stack } from "expo-router";
+import { useCallback } from 'react';
 import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
 
 // export const unstable_settings = {
 //   initialRouteName: "home",
 // };
 
-// const Layout = () => {
-//   const [fontsLoaded] = useFonts({
-//     PTSansRegular: require("../assets/fonts/PTSans-Regular.ttf"),
-//   });
-
-//   if (!fontsLoaded) {
-//     return null;
-//   }
-
-//   return (
-//     <Stack initialRouteName="home">
-//       <Stack.Screen name="home" />
-//     </Stack>
-//   )
-// };
+SplashScreen.preventAutoHideAsync();
 const Layout = () => {
-  return <Stack/>
-}
+  const [fontsLoaded] = useFonts({
+    PTSansRegular: require("../assets/fonts/PTSans-Regular.ttf"),
+    PTSansBold: require("../assets/fonts/PTSans-Bold.ttf"),
+    PTSansItalic: require("../assets/fonts/PTSans-Italic.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack onLayout={onLayoutRootView}/>
+};
 
 export default Layout;
