a23c39fd5bde29809e8b1202f88053e81b08db59
mikesov <minh.bui@monkey.edu.vn>
2023-08-29 08:14:38 +0700

    Override find() from BaseRepository

--- backend/app/Repositories/StoryRepository.php
+++ backend/app/Repositories/StoryRepository.php
@@ -2,8 +2,9 @@
 
 namespace App\Repositories;
 
-use App\Contracts\RepositoryInterfaces\StoryRepositoryInterface;
+use App\Models\Page;
 use App\Models\Story;
+use Illuminate\Http\JsonResponse;
 
 class StoryRepository extends BaseRepository
 {
@@ -15,5 +16,15 @@ class StoryRepository extends BaseRepository
     public function getModel(): string
     {
         return Story::class;
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function find($id): JsonResponse
+    {
+        $story = $this->model->with(['pages'])->where('id', $id)->first();
+        return response()->json($story);
     }
 }
