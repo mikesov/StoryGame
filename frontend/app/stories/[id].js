658f9ac3ecc7d3d9e3e9263034ee814ca43eb668
mikesov <minh.bui@monkey.edu.vn>
2023-08-29 08:18:12 +0700

    Attemp to display API to a specific story screen

--- frontend/app/stories/[id].js
+++ frontend/app/stories/[id].js
@@ -1,21 +1,48 @@
 import { useCallback, useState } from 'react';
 import { View, Text, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react-native';
 import { Stack, useRouter, useGlobalSearchParams } from "expo-router";
-import {useRoute} from "@react-navigation/native";
+import { useRoute } from "@react-navigation/native";
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
 
 import { COLORS, icons, SIZES } from '../../constants';
-import {fetchStory} from '../../hooks';
+import { fetchStory } from '../../hooks';
 
 const Story = () => {
   const params = useGlobalSearchParams();
   const router = useRouter();
   const route = useRoute();
 
-  const { stories, isLoading, error, refetch } = fetchStory("stories", parseInt(params.id));
+  const { story, isLoading, loading, error, refetch } = fetchStory("stories", params.id);
+  const [currentScreen, setCurrentScreen] = useState(0);
+  console.log(loading);
+  // console.log(story.pages[0].id);
   return (
-    <View>
-      <Text>{stories.name}</Text>
-    </View>
+    <SafeAreaView>
+      <Stack.Screen 
+      options={{
+        headerStyle: { backgroundColor: COLORS.lightWhite },
+        headerShadowVisible: false,
+        headerTitle: story.name,
+      }}/>
+      {loading && (<Text>{story.pages[0].id}</Text>)}
+      
+      {/* {story.pages.map((page) => {
+          return <Text style={{ fontSize: 20 }}>{page.id}</Text>;
+        })} */}
+  {/* <SwiperFlatList
+      autoplay
+      autoplayDelay={2}
+      autoplayLoop
+      index={2}
+      showPagination
+      data={colors}
+      renderItem={({ item }) => (
+        <View style={[styles.child, { backgroundColor: item }]}>
+          <Text style={styles.text}>{item}</Text>
+        </View>
+      )}
+    /> */}
+    </SafeAreaView>
   )
 }
 
