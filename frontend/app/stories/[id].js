cd8d01049f41c1bf92fc1941ea2ed2d9576d7345
mikesov <minh.bui@monkey.edu.vn>
2023-08-30 17:32:57 +0700

    Small modification to header

--- frontend/app/stories/[id].js
+++ frontend/app/stories/[id].js
@@ -1,54 +1,37 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { View, Text, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react-native';
-import { Stack, useRouter, useGlobalSearchParams } from "expo-router";
-import { useRoute } from "@react-navigation/native";
-import { SwiperFlatList } from 'react-native-swiper-flatlist';
+import { Stack, useRouter, useGlobalSearchParams } from 'expo-router';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 import { COLORS, icons, SIZES } from '../../constants';
 import { fetchStory } from '../../hooks';
+import { StoryView, ScreenHeaderBtn } from '../../components';
 
 const Story = () => {
-  const params = useGlobalSearchParams();
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
   const router = useRouter();
-  const route = useRoute();
 
-  const { story, isLoading, error, refetch } = fetchStory("stories", params.id);
-  // console.log(story.pages[0].id);
-  if (!story?.pages || story?.pages?.length <= 0) {
-    return (
-      <View>
-        <Text>No pages!</Text>
-      </View>
-    );
-  }
-  console.log(story?.pages);
   return (
     <SafeAreaView>
-      {/* <Stack.Screen 
+      <Stack.Screen 
         options={{
-          headerStyle: { backgroundColor: COLORS.lightWhite },
-          headerShadowVisible: false,
-          headerTitle: story.name,
-      }}/> */}
-      <Text style={{paddingLeft:100}}>{story.pages[0].id}</Text>
-      {/* {story.pages.map((page) => {
-          return <Text style={{ fontSize: 20 }}>{page.id}</Text>;
-        })} */}
-      {/* <SwiperFlatList
-        autoplay
-        autoplayDelay={2}
-        autoplayLoop
-        index={2}
-        showPagination
-        data={story.pages}
-        renderItem={({ item }) => (
-        <View style={{marginTop:20}}>
-          <Text style={{fontSize:20}}>{item.page_id}</Text>
-        </View>
-        )}
-      /> */}
+          headerTransparent: true,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => {
+                ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+                router.back();
+              }}
+            />
+          ),
+          headerTitle: "",
+        }}/>
+      <StoryView/>
     </SafeAreaView>
-  )
+  );
 }
 
 export default Story;
